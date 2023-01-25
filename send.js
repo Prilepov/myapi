39feba86bcb862f622d85efb351483863662f9ce
DiLess <zzloba@gmail.com>
2023-01-25 20:54:04 +0300

    Work v01_02 only NAME

--- send.js
+++ send.js
@@ -2,21 +2,20 @@ function f(){
     let send_id=document.getElementById('id_id').value;
     let send_title=document.getElementById('id_title').value;
     let send_post=document.getElementById('id_post').value;
-    var allData= {
-        "NAME": send_id,
-        "ANOTHER": send_title,
-        "ANOTHER2": send_post
-    };
+
+    var formData = new FormData();
+
+    formData.append("fields[NAME]", send_id);
     
-    console.log(allData);
+    console.log(formData);
 
     $.ajax({
-        url: "https://samcorp.bitrix24.ru/rest/1/qlsvk0nj5fwemx01/crm.product.add.json",
+        url: "https://samcorp.bitrix24.ru/rest/1/pdc55j0gh5l79klf/crm.product.add.json",
         type: "post", 
         dataType: "json", 
         processData: false,
         contentType: false,
-        data: allData,
+        data: formData,
         error:function(XMLHttpRequest, textStatus, errorThrown) {
             console.log("Status: " + textStatus); alert("Error: " + errorThrown);
         }
